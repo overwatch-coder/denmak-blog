7b27a8f3d843e3a3b505b3aef210edb12b9458a6
Nyamadi Atsu <devbrainy98@gmail.com>
2022-11-14 18:31:30 +0100

    added frontend or client side files

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+const  App = () => {
+  return (
+    <div className="bg-cyan-800 text-white text-3xl">
+      <p className="text-center">Hello World</p>
+      <p className="text-6xl mt-10 text-white"
+      >This is my home</p>
+    </div>
+  )
+}
+
+export default App
